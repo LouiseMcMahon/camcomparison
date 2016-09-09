4d241d37a41a0a2e544faf8263d1eb1fade8290b
Louise McMahon <louise@louisemcmahon.co.uk>
2016-09-09 15:02:13 +0100

    reformated events.js

--- js/events.js
+++ js/events.js
@@ -1,133 +1,133 @@
-function defineEvents(){
+function defineEvents() {
 	var camTable = $('#camTable').DataTable();
 
-	camTable.on( 'draw', function () {
-		buildRangeCollum()
-		fillAdvancedSearchOptions()
-	} );
-	
-	$( window ).resize(function() {
-		buildRangeCollum()
+	camTable.on('draw', function () {
+		buildRangeCollum();
+		fillAdvancedSearchOptions();
 	});
-	
-	$('#manufacturerSearchOption').on( 'change', function () {
-		camTable
-		.column(0)
-		.search( $(this).val() )
-		camTable
-		.column(1)
-		.search("")
-		camTable
-		.column(2)
-		.search("")				
-		
-	    camTable.draw();
-	} );
-	
-	$('#modelSearchOption').on( 'change', function () {
-		camTable
-		.column(1)
-		.search( $(this).val() )				
-		camTable
-		.column(2)
-		.search("")
-	    
-		camTable.draw();
-	} );
-	
-	$('#nameSearchOption').on( 'change', function () {
-		camTable
-		.column(2)
-		.search( $(this).val() )
-	    .draw();
-	} );		    
-		   		
-	
-	$('#rangeInput').keyup( function() {
-		camTable.draw();
-	} );
 
-	$('#rangeMin').keyup( function() {
-		camTable.draw();
-	} );
+	$(window).resize(function () {
+		buildRangeCollum();
+	});
 
-	$('#rangeMax').keyup( function() {
-		camTable.draw();
-	} );
-	
-	$('#priceInput').keyup( function() {
-		camTable.draw();
-	} );
-	
-	$('#weightInput').keyup( function() {
-		camTable.draw();
-	} );
-	
-	$('#strengthInput').keyup( function() {
-		camTable.draw();
-	} );
-	
-	$('#stemCountInput').on( 'change', function () {
+	$('#manufacturerSearchOption').on('change', function () {
 		camTable
-		.column(7)
-		.search( $(this).val() )
-	    .draw();
-	} );
-	
-	$('#axleCountInput').on( 'change', function () {
+			.column(0)
+			.search($(this).val());
 		camTable
-		.column(8)
-		.search( $(this).val() )
-	    .draw();
-	} );
+			.column(1)
+			.search('');
+		camTable
+			.column(2)
+			.search('');
 
-	$('#lobeCountInput').on( 'change', function () {
+		camTable.draw();
+	});
+
+	$('#modelSearchOption').on('change', function () {
+		camTable
+			.column(1)
+			.search($(this).val());
+		camTable
+			.column(2)
+			.search('');
+
+		camTable.draw();
+	});
+
+	$('#nameSearchOption').on('change', function () {
+		camTable
+			.column(2)
+			.search($(this).val())
+			.draw();
+	});
+
+
+	$('#rangeInput').keyup(function () {
+		camTable.draw();
+	});
+
+	$('#rangeMin').keyup(function () {
+		camTable.draw();
+	});
+
+	$('#rangeMax').keyup(function () {
+		camTable.draw();
+	});
+
+	$('#priceInput').keyup(function () {
+		camTable.draw();
+	});
+
+	$('#weightInput').keyup(function () {
+		camTable.draw();
+	});
+
+	$('#strengthInput').keyup(function () {
+		camTable.draw();
+	});
+
+	$('#stemCountInput').on('change', function () {
+		camTable
+			.column(7)
+			.search($(this).val())
+			.draw();
+	});
+
+	$('#axleCountInput').on('change', function () {
+		camTable
+			.column(8)
+			.search($(this).val())
+			.draw();
+	});
+
+	$('#lobeCountInput').on('change', function () {
 		camTable
 			.column(9)
-			.search( $(this).val() )
+			.search($(this).val())
 			.draw();
-	} );
-	
-	$('#extendableSlingInput').on( 'change', function () {
+	});
+
+	$('#extendableSlingInput').on('change', function () {
 		camTable
-		.column(10)
-		.search( $(this).val() )
-	    .draw();
-	} );
-	
-	$('#offsetInput').on( 'change', function () {
+			.column(10)
+			.search($(this).val())
+			.draw();
+	});
+
+	$('#offsetInput').on('change', function () {
 		camTable
-		.column(11)
-		.search( $(this).val() )
-	    .draw();
-	} );
-	
-	$('#showExtraData').on( 'click', function () {
-		
-		console.log(camTable.columns(5).visible()[0])
-		if (camTable.columns(5).visible()[0]){
+			.column(11)
+			.search($(this).val())
+			.draw();
+	});
+
+	$('#showExtraData').on('click', function () {
+
+		console.log(camTable.columns(5).visible()[0]);
+		if (camTable.columns(5).visible()[0]) {
 			camTable
-			.columns([5,6,7,8,9,10])
-			.visible(false)
-			$('#showExtraData').html("Show </br>Advanced Search")
+				.columns([5, 6, 7, 8, 9, 10])
+				.visible(false);
+			$('#showExtraData').html('Show </br>Advanced Search')
 		}
-		else{
+		else {
 			camTable
-			.columns([5,6,7,8,9,10])
-			.visible(true)
-			$('#showExtraData').html("Hide </br>Advanced Search")
+				.columns([5, 6, 7, 8, 9, 10])
+				.visible(true);
+			$('#showExtraData').html('Hide </br>Advanced Search')
 		}
-		buildRangeCollum()
-		$('#weightInput').parent().toggle()
-		$('#strengthInput').parent().toggle()
-		$('#stemCountInput').parent().toggle()
-		$('#axleCountInput').parent().toggle()
-		$('#lobeCountInput').parent().toggle()
-		$('#extendableSlingInput').parent().toggle()
-		$('#offsetInput').parent().toggle()
-	} );
-	
-	$('#currencyInput').on( 'change', function () {
-		importData()
-	} );
+		buildRangeCollum();
+		$('#weightInput').parent().toggle();
+		$('#strengthInput').parent().toggle();
+		$('#stemCountInput').parent().toggle();
+		$('#axleCountInput').parent().toggle();
+		$('#lobeCountInput').parent().toggle();
+		$('#extendableSlingInput').parent().toggle();
+		$('#offsetInput').parent().toggle();
+	});
+
+	$('#currencyInput').on('change', function () {
+		importData();
+	});
 }
